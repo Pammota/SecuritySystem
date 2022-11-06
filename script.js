5ccac31f58c7212aeee1fa21352b731b16e371b9
Pammota <gligor.bogdan@campeni.ro>
2022-11-06 22:39:17 +0200

    updates

--- script.js
+++ script.js
@@ -1,8 +1,26 @@
+function delay(time) {
+  return new Promise((resolve) => setTimeout(resolve, time));
+}
+
 const play = () => {
-    const audio = new Audio('https://assets.mixkit.co/sfx/preview/mixkit-elevator-tone-2863.mp3');
-    audio.play();
+  const audio = new Audio(
+    "https://assets.mixkit.co/sfx/preview/mixkit-elevator-tone-2863.mp3"
+  );
+  audio.play();
 };
 
-$( "#buttonpress" ).click(function() {
+$("#buttonpress").click(async () => {
+  const alertron = async (time) => {
+    await delay(time);
     play();
+    await delay(time);
+    play();
+    await delay(time);
+    play();
+    await delay(time*2);
+  };
+
+  await alertron(500);
+  await alertron(500);
+  await alertron(500);
 });
