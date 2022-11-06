e1a57d2828af2b2f454369662562b5925925abb0
Pammota <gligor.bogdan@campeni.ro>
2022-11-06 22:26:09 +0200

    updates

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
-alert( "INIT" );
+const play = () => {
+    const audio = new Audio('https://assets.mixkit.co/sfx/preview/mixkit-elevator-tone-2863.mp3');
+    audio.play();
+};
 
 $( "#buttonpress" ).click(function() {
-    alert( "YOU PRESSED ME" );
-  });
+    play();
+});
