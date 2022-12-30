733f4624f0f730118b36f94135e0da86b386de1a
Pammota <gligor.bogdan@campeni.ro>
2022-12-30 23:40:44 +0200

    update

--- moveAPI/test.js
+++ moveAPI/test.js
@@ -1,23 +1,57 @@
 const Gpio = require("onoff").Gpio;
 const LED1 = new Gpio(2, "out");
+const LED2 = new Gpio(4, "out");
+const LED3 = new Gpio(17, "out");
+let state = 2;
 
-const pir = new Gpio(21, "in", "both");
+const pir = new Gpio(27, "in", "both");
 
-const sensor = (err, value) => {
+pir.watch(function(err, value) {
   if (err) exit();
+  let blinkInterval = setInterval(blinkLED, 250);
 
   console.log("Intruder detected");
+  console.log("Pi Bot deployed successfully!");
+  console.log("Guarding the raspberry pi 3...");
 
-  setTimeout(endBlink, 10);
+  setTimeout(endBlink, 15000);
 
   function endBlink() {
+    clearInterval(blinkInterval);
+    LED1.writeSync(0);
+    LED1.unexport();
+    LED2.writeSync(0);
+    LED2.unexport();
+    LED3.writeSync(0);
+    LED3.unexport();
+
     //included when we are working with sensors
     pir.unexport();
     process.exit();
   }
-};
+});
 
-
-while (true) {
-  setTimeout(()=> pir.watch(sensor),200);
+function blinkLED() {
+  if (state == 2) {
+    if (LED1.readSync() === 0) {
+      LED1.writeSync(1);
+    } else {
+      LED1.writeSync(0);
+      state = 4;
+    }
+  } else if (state == 4) {
+    if (LED2.readSync() === 0) {
+      LED2.writeSync(1);
+    } else {
+      LED2.writeSync(0);
+      state = 6;
+    }
+  } else {
+    if (LED3.readSync() === 0) {
+      LED3.writeSync(1);
+    } else {
+      LED3.writeSync(0);
+      state = 2;
+    }
+  }
 }
