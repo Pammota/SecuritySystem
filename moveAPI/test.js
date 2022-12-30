f8702c3f14c4f9a3d5f19c3c53babf7a3706472b
Pammota <gligor.bogdan@campeni.ro>
2022-12-30 23:27:34 +0200

    update

---
+++ moveAPI/test.js
@@ -0,0 +1,27 @@
+const Gpio = require("onoff").Gpio;
+const LED1 = new Gpio(2, "out");
+let state = 2;
+
+const pir = new Gpio(21, "in", "both");
+
+pir.watch(function(err, value) {
+  if (err) exit();
+  let blinkInterval = setInterval(blinkLED, 250);
+
+  console.log("Intruder detected");
+  console.log("Pi Bot deployed successfully!");
+  console.log("Guarding the raspberry pi 3...");
+
+  setTimeout(endBlink, 15000);
+
+  function endBlink() {
+
+    //included when we are working with sensors
+    pir.unexport();
+    process.exit();
+  }
+});
+
+function blinkLED() {
+  console.log("Blinked");
+}
