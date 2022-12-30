4c3982bf49fcd16789f188d230036a4785c30dcf
Pammota <gligor.bogdan@campeni.ro>
2022-12-31 00:11:02 +0200

    update

--- moveAPI/test.js
+++ moveAPI/test.js
@@ -1,6 +1,6 @@
 const Gpio = require("onoff").Gpio;
 
-const pir = new Gpio(21, "in", "rising");
+const pir = new Gpio(21, "in", "both");
 
 let activating = 0;
 
@@ -8,7 +8,7 @@ pir.watch(function(err, value) {
   if (err) exit();
   //let blinkInterval = setInterval(blinkLED, 250);
 
-  console.log(`Intruder nr. ${activating++} detected`);
+  console.log(`Intruder nr. ${activating++} detected, value ${value}`);
 
   setTimeout(endBlink, 2000);
 
