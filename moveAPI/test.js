ece50a2e1c8bcdc4b969745700cc214def0186f9
Pammota <gligor.bogdan@campeni.ro>
2022-12-30 23:47:03 +0200

    update

--- moveAPI/test.js
+++ moveAPI/test.js
@@ -3,13 +3,13 @@ const LED1 = new Gpio(2, "out");
 
 const pir = new Gpio(21, "in", "both");
 
+let activating = 0;
+
 pir.watch(function(err, value) {
   if (err) exit();
   //let blinkInterval = setInterval(blinkLED, 250);
 
-  console.log("Intruder detected");
-  console.log("Pi Bot deployed successfully!");
-  console.log("Guarding the raspberry pi 3...");
+  console.log(`Intruder nr. ${activating++} detected`);
 
   setTimeout(endBlink, 2000);
 
