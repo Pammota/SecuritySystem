f15253190cf27e670998d36b17b478b624268b29
Pammota <gligor.bogdan@campeni.ro>
2023-01-13 04:47:32 +0200

    update

--- raspberrypi_node_camera_web_streamer/public/script.js
+++ raspberrypi_node_camera_web_streamer/public/script.js
@@ -9,7 +9,7 @@ const play = () => {
   audio.play();
 };
 
-$("#buttonpress").click(async () => {
+const alertSound = async () => {
   const alertron = async (time) => {
     await delay(time);
     play();
@@ -17,10 +17,33 @@ $("#buttonpress").click(async () => {
     play();
     await delay(time);
     play();
-    await delay(time*2);
+    await delay(time * 2);
   };
 
   await alertron(500);
   await alertron(500);
   await alertron(500);
-});
+};
+
+// const isAlarmed = () => {
+//   fetch("http://codepi.local:3000/api/true-or-false")
+//     .then((response) => response.json())
+//     .then((data) => console.log(data.value));
+// };
+
+const myPeriodicMethod = () => {
+  $.ajax({
+    url: "http://codepi.local:3000/api/true-or-false",
+    success: (data) => {
+      if(data.value)
+        alertSound();
+      console.log(data.value);
+    },
+    complete: () => {
+      // schedule the next request *only* when the current one is complete:
+      setTimeout(myPeriodicMethod, 500);
+    },
+  });
+};
+
+myPeriodicMethod();
